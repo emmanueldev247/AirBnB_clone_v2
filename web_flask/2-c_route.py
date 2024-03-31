37fe8cb49f1bddf14ac3c11401fd52d7a13f79ba
Emmanuel Ademola <mailemmydee@gmail.com>
2024-03-30 20:13:14 -0700

    added: C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    return "C {}".format(text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
