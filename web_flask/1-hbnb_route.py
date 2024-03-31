bdb176631bb5b53aecf57549e9911dc1727e7d7f
Emmanuel Ademola <mailemmydee@gmail.com>
2024-03-30 17:23:55 -0700

    added: HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
